ed1f8038b1430bff054876c2e1334164e155a9e8
Ben Demaree <bendemaree@gmail.com>
2012-03-30 11:11:54 -0400

    Created installation script.

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+while true;
+do
+        echo "You are about to install nginxtools.  Continue? [Y/n] " yn
+        case $yn in
+                [Yy]*   ) break;;
+                [Nn]*   ) exit 1;;
+                *       ) "Entry not recognized.  Please enter Y or n.";;
+        esac
+done
+
+if [[ $EUID -ne "0" ]]
+then
+        echo "You are not running as root.  You will be prompted for your password."
+fi
+
+echo "Beginning install..."
+wget https://raw.github.com/bendemaree/nginxtools/master/scripts/nginxtools.sh
+echo ""
+
+echo "Moving scripts into place..."
+sudo mv nginxtools.sh /usr/local/bin/nginxtool
+
+echo "Setting permissions..."
+sudo chmod +x /usr/local/bin/nginxtool
+
+echo "Verifying installation..."
+command -v nginxtool >/dev/null || {
+        echo "Installation failed...perhaps /usr/bin/local is not included in your PATH variable."
+        exit 1
+}
+
+echo "Installation complete!  You are now able to use nginxtool."
+rm install.sh
+exit 0
